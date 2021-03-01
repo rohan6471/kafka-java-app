133a6d71a996afc5cae69a41472824a3986ba7b7
rohan6471 <54380500+rohan6471@users.noreply.github.com>
2021-02-28 21:18:36 -0600

    added resources/log4j.properties

--- src/main/java/edu/nwmissouri/bigdata/group6/App.java
+++ src/main/java/edu/nwmissouri/bigdata/group6/App.java
@@ -1,4 +1,5 @@
 package edu.nwmissouri.bigdata.group6;
+import java.util.*;
 
 /**
  * Hello world!
@@ -8,6 +9,49 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+    	System.out.println("");
     }
-}
+    }
+//    	Scanner scan=new Scanner(System.in);
+//    	System.out.println("Enter Y or N to start guess number game: ");
+//    	String s=scan.nextLine();
+//    	while(s.equals("y")){
+//    		Random r=new Random();
+//    		int a=r.nextInt(50-10)+10;
+//    		int[] num_guess= {1,23,32,12,33,23,14,65};
+//    		System.out.println("what is your guess?");
+//    		int b=scan.nextInt();
+//    		if(a==b) {
+//    			System.out.println("your guess is correct");
+//    		}
+//    		else {
+//    			System.out.println("sorry wrong guess! the number generated is"+a);
+//    		}
+//    		System.out.println("Enter Y or N to start guess number game: ");
+//    		s=scan.next();
+//    	}
+//    	System.out.println( "Thank you!" );
+//    	for(int i=0;i<10;i++) {
+//    		String message=guessNumber();
+//    		System.out.println(message);
+//    		
+//    	}
+//    }
+    
+//    private static String guessNumber() {
+//        int[] num_guess= {1,23,32,12,33,23,14,44,43,17};
+//        String str;
+//    	Random r=new Random();
+//    	int guessed_num=r.nextInt(50-10)+10;
+//    	int generated_num=r.nextInt(10-1)+1;
+//    	if(num_guess[generated_num]==guessed_num){
+//    		str="your guessed the correct number";
+//    	}
+//    	else{
+//    		str="Sorry! wrong guess. The number generated is "+num_guess[generated_num];
+//    	}
+//    	return str;
+//
+//        }
+//}
